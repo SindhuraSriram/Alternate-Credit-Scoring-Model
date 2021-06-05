6e0529e1be8892ad5e260440d40f1b2ae632c833
SindhuraSriram <42845151+SindhuraSriram@users.noreply.github.com>
2021-06-05 16:45:00 +0530

    Add files via upload

---
+++ defines.py
@@ -0,0 +1,34 @@
+#from enum import enum
+import json
+
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    reverse = dict((value, key) for key, value in enums.items())
+    enums['reverse_mapping'] = reverse
+    return type('Enum', (), enums)
+
+Types = enum('NUMERICAL',
+             'CATEGORICAL_BINARY',
+             'CATEGORICAL_ORDERED',
+             'CATEGORICAL_UNORDERED')
+
+class Column(object):
+    def __init__(self, attribute=None, header='', type=None, categories=None):
+        self.ATTRIBUTE = attribute
+        self.HEADER = header
+        self.TYPE = type
+        self.CATEGORIES = categories
+
+class Metadata(object):
+    def __init__(self):
+        metadata = json.load(open('metadata.json', 'r'))
+        self.NAME = metadata['NAME']
+        self.LOCATION = metadata['LOCATION']
+        self.IS_INDEXED = metadata['IS_INDEXED']
+        self.IS_LABELED = metadata['IS_LABELED']
+        self.COLUMNS = []
+        for column in metadata['COLUMNS']:
+            self.COLUMNS.append(Column(attribute=column['attribute'],
+                                       header=column['header'],
+                                       type=getattr(Types, column['type']),
+                                       categories=column['categories']))
